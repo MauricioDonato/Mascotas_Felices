25778268ed0715d9e9917247cf2a88daf4baed9b
MauricioDonato <mau.donato@duocuc.cl>
2023-10-06 18:56:12 -0300

    Inicio de Proyecto

---
+++ src/main/java/com/mycompany/mascotasfelices/MascotasFelices.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mascotasfelices;
+
+/**
+ *
+ * @author mauri
+ */
+public class MascotasFelices {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
